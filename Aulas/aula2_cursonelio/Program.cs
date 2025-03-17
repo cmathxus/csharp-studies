8eb7e527a087d61697a4e0f5fc261f7d2920234a
Caio Matheus <kirodnv@gmail.com>
2025-03-17 17:04:44 -0300

    Renomeando os arquivos das aulas 'tirando o _cursonelio'

--- Aulas/aula2_cursonelio/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace aula2_cursonelio;
-
-class Program{
-static void Main (string [] args) {
-    Console.WriteLine("Teste aula 2");
-    }
-}
---
+++ Aulas/aula3/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace aula3_cursonelio;
+class Program{
+static void Main (string [] args) {
+    System.Console.WriteLine("Hello world!");
+    }
+}
