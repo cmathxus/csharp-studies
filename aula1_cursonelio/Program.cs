43ada01fbc3ab02f237b6d5d7933fab1ab1cc436
Caio Matheus <kirodnv@gmail.com>
2025-03-17 15:33:04 -0300

    add README.md

---
+++ aula1_cursonelio/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace aula1_cursonelio {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Boa tarde!");
+            Console.WriteLine("Boa noite!");
+        }
+    }
+}
