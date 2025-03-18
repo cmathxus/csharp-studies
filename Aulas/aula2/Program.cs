25fc3097ebb33930bc4ba7e88fc27cdea2d7c3fc
Caio Matheus <kirodnv@gmail.com>
2025-03-18 16:32:55 -0300

    Atualizando aulas

--- Aulas/aula2/Program.cs
+++ Aulas/aula2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace aula2;
+class Program {
+    static void Main (string [] args){
+        int a = 10;
+        System.Console.WriteLine(a);
+
+        a += 2;
+        System.Console.WriteLine(a);
+
+        a *= 3;
+        System.Console.WriteLine(a);
+
+        string s = "ABC";
+        System.Console.WriteLine(s);
+
+        s += "DEF";
+        System.Console.WriteLine(s);
+
+        int b = 10;
+
+        b++; // b = b+1
+
+        System.Console.WriteLine(b);
+
+    }
+}
