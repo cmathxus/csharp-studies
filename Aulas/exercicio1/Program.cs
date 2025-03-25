495b3c0e5f5d365e4c8da903706be9a56382b73d
Caio Matheus <kirodnv@gmail.com>
2025-03-25 16:51:35 -0300

    Atualizando aulas

---
+++ Aulas/exercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou positivo
+
+using System;
+using System.Reflection;
+
+namespace exercicio1{
+    class Program {
+        static void Main (String [] args) {
+            System.Console.WriteLine("Digite o número: ");
+            double num = Double.Parse(Console.ReadLine());
+
+            if (num >= 0) {
+                System.Console.WriteLine("Positivo");
+            }
+            else {
+                System.Console.WriteLine("Negativo");
+            }
+        }
+    }
+}
