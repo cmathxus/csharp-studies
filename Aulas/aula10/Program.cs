fb0cb80fc01f3e3f4825ff7498053eac3787b77f
Caio Matheus <kirodnv@gmail.com>
2025-03-26 22:27:48 -0300

    Atualizando aulas

---
+++ Aulas/aula10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace aula10 {
+    class Program {
+        static void Main (String [] args) {
+
+            System.Console.WriteLine("Digite três números");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            double resultado = Maior(n1, n2, n3);
+
+            System.Console.WriteLine("Maior = ", resultado);
+
+
+            }
+
+            static int Maior(int a, int b, int c) {
+                int m;
+                if (a > b && a > c) {
+                    m = a;
+                }
+                else if (b > c) {
+                    m = b;
+                }
+                else {
+                    m = c;
+                }
+                return m;
+        }
+    }
+}
