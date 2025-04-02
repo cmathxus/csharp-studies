eb4044c5e4f971ed7056d6de929014f8470f0256
Caio Matheus <kirodnv@gmail.com>
2025-04-02 10:43:34 -0300

    Atualizando aulas

---
+++ Aulas/aula11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace aula11{
+    class Program {
+        static void Main(String [] args) {
+
+            System.Console.Write("Digite um número ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+  
+            while (x >= 0.0) {
+                double raiz = Math.Sqrt(x);
+                System.Console.WriteLine(raiz.ToString("F3",CultureInfo.InvariantCulture));
+                System.Console.WriteLine("Digite outro número ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            
+            System.Console.WriteLine("Número negativo");
+        }
+    }
+}
