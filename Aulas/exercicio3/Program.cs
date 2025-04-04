e4d22c4896d64f31159c519a799818fb9de60566
Caio Matheus <kirodnv@gmail.com>
2025-04-04 10:32:44 -0300

    Atualizando aulas

--- Aulas/exercicio3/Program.cs
+++ Aulas/exercicio3/Program.cs
@@ -8,7 +8,7 @@ namespace exercicio3 {
             System.Console.WriteLine("Entre com os dados do produto: ");
             System.Console.Write("Nome: ");
             produto1.Nome = Console.ReadLine();
-            System.Console.Write("Preco: ");
+            System.Console.Write("Preco: $");
             produto1.Preco = int.Parse(Console.ReadLine());
             System.Console.Write("Quantidade: ");
             produto1.Quantidade = int.Parse(Console.ReadLine());
--- Aulas/exercicio3/obj/Debug/net9.0/exercicio3.AssemblyInfo.cs
+++ Aulas/exercicio3/obj/Debug/net9.0/exercicio3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercicio3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+225121757bfb47e7c704ce8b44085437cc751138")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14d7c300d867e1f86d1802a279b353fcf8bf04e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercicio3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercicio3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
