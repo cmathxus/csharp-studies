f7ea0d13d26ac90dd8cea3427cbcbabda2864703
Caio Matheus <kirodnv@gmail.com>
2025-04-04 10:30:09 -0300

    Atualizando aulas

--- Aulas/exercicio3/Program.cs
+++ Aulas/exercicio3/Program.cs
@@ -5,9 +5,13 @@ namespace exercicio3 {
         static void Main (String [] args) {
             Product produto1 = new Product ();
 
-            produto1.Nome = "Coca cola";
-            produto1.Preco = 5;
-            produto1.Quantidade = 30;
+            System.Console.WriteLine("Entre com os dados do produto: ");
+            System.Console.Write("Nome: ");
+            produto1.Nome = Console.ReadLine();
+            System.Console.Write("Preco: ");
+            produto1.Preco = int.Parse(Console.ReadLine());
+            System.Console.Write("Quantidade: ");
+            produto1.Quantidade = int.Parse(Console.ReadLine());
 
             produto1.printer();
 
