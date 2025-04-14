13f307cae4e4a74e70b7f75564a135dcb82711d6
Caio Matheus <kirodnv@gmail.com>
2025-04-14 12:16:30 -0300

    Atualizando aulas

--- Aulas/aula18/Program.cs
+++ Aulas/aula18/Program.cs
@@ -3,9 +3,13 @@
 namespace aula18 {
     class Program {
         static void Main (String [] args) {
-            Point p;
+            Point p; // tipo struct não necessita do comando = new, pois ele apenas copia o valor (não faz uma referencia a caixa de memoria)
+            p.X = 10;
+            p.Y = 20; // necessario sempre inicializar os valores da variavel, no caso de não inicializar vai retornar erro de "variavel nao atribuida"
+            System.Console.WriteLine(p); // valor (10,20)
 
-            System.Console.WriteLine(p);
+            p = new Point(); 
+            System.Console.WriteLine(p); // valor (0,0), pois ele esta copiando o novo valor padrao da variavel
         }
     }
 }
