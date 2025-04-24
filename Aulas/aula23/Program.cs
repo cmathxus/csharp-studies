fa75df99135853d4abb59b2c0d55bc2a157aae1a
Caio Matheus <kirodnv@gmail.com>
2025-04-23 22:15:11 -0300

    Atualizando aulas

---
+++ Aulas/aula23/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace aula23 {
+    class Program {
+        static void Main(string [] args) {
+
+            string[] vect = new string[] {"Maria", "Alex", "Bob"};
+
+            foreach (string obj in vect) {
+                System.Console.WriteLine(obj); // Utilizando o foreach para percorrer o vetor e imprimir os valores
+            } 
+        }
+    }
+}
